1db32b17f1b7ad1b2538f56589d4595dd8eb075b
JonShard <jonems@stud.ntnu.no>
2019-01-24 12:26:32 +0100

    Added wall observer and game manager.

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+	public droid.Runtime.Prototyping.Observers.WallObserver wallObserver;
+	public List<Transform> walls;
+    
+    void Start()
+    {
+    	foreach (Transform trans in transform.Find("Obstacles"))
+    	{
+    		walls.Add(trans);
+    	}    
+    }
+
+    void RegisterResetWall(bool[] side)
+    {
+    	float[] shiftBuffer = new float[walls.Count-4];
+    	for (int i = 0, j = shiftBuffer.Length-1 -side.Length; j < shiftBuffer.Length; i++, j++)
+    	{
+    		shiftBuffer[j] = (side[i] ? 1 : 0);
+    	}
+    	wallObserver.wallStates = new List<float>(shiftBuffer);
+    }
+}
---
+++ Assets/Scripts/WallObserver.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using droid.Runtime.Interfaces;
+using droid.Runtime.Utilities.Structs;
+
+
+namespace droid.Runtime.Prototyping.Observers
+{
+
+    public class WallObserver : Observer,
+                            IHasArray 
+    {
+
+        public List<float> wallStates = new List<float>();
+        public float[] ObservationArray => wallStates.ToArray();
+        public override void UpdateObservation() {  }
+
+    }
+}
